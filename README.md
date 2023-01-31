358c80d91ded91e68aa4f6ed385c75ca1b1757c5
Therry van Neerven <therry.van.neerven@curiousbits.nl>
2023-01-31 17:30:37 +0100

    feat: basic implementation to download loom videos
    This is a very basic implementation which allows users to download loom videos.
    It only requires standard Python dependencies.
    Tested manually with Python 3.10 on Ubuntu 22.04.

---
+++ README.md
@@ -0,0 +1,9 @@
+# loom-dl
+
+This script allows you to download public loom videos as a `.mp4` file so that you can watch them when being offline.
+
+## Usage
+
+```shell
+$ ./loom-dl.py https://www.loom.com/share/... -o out/video.mp4
+```
---
+++ loom-dl.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import argparse
+import json
+import urllib.request
+
+
+def fetch_loom_download_url(id):
+    request = urllib.request.Request(
+        url=f"https://www.loom.com/api/campaigns/sessions/{id}/transcoded-url",
+        headers={},
+        method="POST",
+    )
+    response = urllib.request.urlopen(request)
+    body = response.read()
+    content = json.loads(body.decode("utf-8"))
+    url = content["url"]
+    return url
+
+
+def download_loom_video(url, filename):
+    urllib.request.urlretrieve(url, filename)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog="loom-dl", description="script to download loom.com videos"
+    )
+    parser.add_argument(
+        "url", help="Url of the video in the format https://www.loom.com/share/[ID]"
+    )
+    parser.add_argument("-o", "--out", help="Path to output the file to")
+    arguments = parser.parse_args()
+    return arguments
+
+
+def extract_id(url):
+    return url.split("/")[-1]
+
+
+def main():
+    arguments = parse_arguments()
+    id = extract_id(arguments.url)
+
+    url = fetch_loom_download_url(id)
+    filename = arguments.out or f"{id}.mp4"
+    print(f"Downloading video {id} and saving to {filename}")
+    download_loom_video(url, filename)
+
+
+if __name__ == "__main__":
+    main()
